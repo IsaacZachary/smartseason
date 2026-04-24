cc27bf608196030f519a1dc91e700811580260bc
IsaacZachary <isaaczachary18@gmail.com>
2026-04-25 00:06:11 +0300

    fix: add path awareness to wsgi for monorepo stability

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,6 +1,14 @@
 import os
+import sys
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
+
+# Add the backend directory to the sys.path
+path = Path(__file__).resolve().parent
+if str(path) not in sys.path:
+    sys.path.append(str(path))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+app = application
