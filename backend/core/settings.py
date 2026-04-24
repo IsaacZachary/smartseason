52b68eee9c54bfe7677bea9ba848371d4041d033
IsaacZachary <isaaczachary18@gmail.com>
2026-04-24 23:53:31 +0300

    fix: relocate wsgi.py and enable debug for deployment troubleshooting

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-vbah1=!d&ssrop+wzt_%&f2z$589!25y+v1*@g2m+1p)jm%974
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
